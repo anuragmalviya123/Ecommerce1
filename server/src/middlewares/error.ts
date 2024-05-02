a0da2ce5bdda569343da5b885dfe9e35c2810798
Anurag Malviya <anuragmalviya62@gmail.com>
2024-05-02 22:18:57 +0530

    done all user interface

--- server/src/middlewares/error.ts
+++ server/src/middlewares/error.ts
@@ -8,7 +8,7 @@ export const errorMiddleware = (
   res: Response,
   next: NextFunction
 ) => {
-  err.message ||= "Internal Server Error";
+  err.message ||= "Internal Servr Error";
   err.statusCode ||= 500;
 
   if (err.name === "CastError") err.message = "Invalid ID";
